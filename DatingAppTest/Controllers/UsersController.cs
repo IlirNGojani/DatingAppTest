66c948b1183db23bd216f3ea2e518e26b30cfe33
ilirg <ilirg@cacttus.com>
2021-02-17 15:40:10 +0100

    commit

---
+++ DatingAppTest/Controllers/UsersController.cs
@@ -0,0 +1,53 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController, Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AppUser>> Create(AppUser item)
+        {
+            await _context.Users.AddAsync(item);
+            await _context.SaveChangesAsync();
+
+            return Created("", item);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<AppUser>> Delete(int id)
+        {
+            AppUser user = await _context.Users.FindAsync(id);
+            if (user == null) return NotFound();
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
